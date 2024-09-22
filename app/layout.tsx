791aca1175ce198f4ab5798283ab0bcc3035f7ae
Nandini Jindal <nandinijindal321@gmail.com>
2024-09-22 17:06:04 +0530

    eslint errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import '../app/globals.css'
 import PageHeader from '@/components/page-header'
+import Script from 'next/script'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          <Script src='https://saas-rouge.vercel.app/'></Script> 
+        </head>
         <body>
         <PageHeader />
         {children}
